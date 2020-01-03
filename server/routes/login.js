a4f05c9eee367d9b8a6fa76e887d25887fd52745
cam-arayaf <camiloaraya1@gmail.com>
2020-01-03 17:38:26 -0300

    modified credential validation message

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,9 +7,10 @@ const app = express();
 app.post('/login', (req, resp) => {
     let body = req.body;
     User.findOne({ email: body.email }, (err, userDB) => {
+        const message = 'Incorrect email or password';
         if (err) return resp.status(500).json({ ok: false, err });
-        if (!userDB) return resp.status(400).json({ ok: false, error: { message: 'Incorrect (email) or password' } });
-        if (!bcrypt.compareSync(body.password, userDB.password)) return resp.status(400).json({ ok: false, error: { message: 'Incorrect email or (password)' } });
+        if (!userDB) return resp.status(400).json({ ok: false, error: { message } });
+        if (!bcrypt.compareSync(body.password, userDB.password)) return resp.status(400).json({ ok: false, error: { message } });
         let token = jwt.sign({
             user: userDB
         }, process.env.SEED, { expiresIn: process.env.TOKEN_EXPIRATION });
