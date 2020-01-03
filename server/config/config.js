6a930cd2c8d61fe27159d4a4804fdba6946463ed
cam.arayaf <camiloaraya1@gmail.com>
2020-01-03 00:24:56 -0300

    Enviroment variable MongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,3 @@
-const dev_url_db = 'mongodb://localhost:27017/coffee';
-const prod_url_db = 'mongodb+srv://cam-arayaf:RQlIWwKHqyTymr9G@cluster0-uir0v.mongodb.net/coffee';
 process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-process.env.URL_DB = process.env.NODE_ENV === 'dev' ? dev_url_db : prod_url_db;
+process.env.URL_DB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/coffee' : process.env.MONGO_URI;
